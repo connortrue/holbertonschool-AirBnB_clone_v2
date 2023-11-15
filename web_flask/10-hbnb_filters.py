ae5bc1d44e5ba12ca4c269e83bf0eaeed85795de
connortrue <connor.true.94@gmail.com>
2023-11-14 22:42:40 -0600

    done

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage, State, Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    amenities = sorted(storage.all(Amenity).values(),
+                       key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
