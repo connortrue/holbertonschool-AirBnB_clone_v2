e9ebd6435b6e99a03934aede1b819d5ca500a75f
connortrue <connor.true.94@gmail.com>
2023-11-14 23:25:10 -0600

    comms

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
+# Import necessary modules from flask and models
 from flask import Flask, render_template
-from models import storage, State
+from models import storage, State, Amenity
 
+# Create an instance of the Flask class
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def states():
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('states.html', states=states)
+# Define a route for '/hbnb_filters' with strict_slashes set to False
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    # Retrieve all State objects, sort them by name, and store them in 'states'
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    amenities = sorted(storage.all(Amenity).values(),
+                       key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
-    state = storage.get(State, id)
-    if state:
-        cities = sorted(state.cities, key=lambda city: city.name)
-        return render_template('state.html', state=state, cities=cities)
-    else:
-        return render_template('404.html'), 404
-
-
+# Define a function to be called when cleaning up after a response
 @app.teardown_appcontext
 def teardown_db(exception):
+    # Close the storage
     storage.close()
 
 
+# Check if the script is run directly (not imported)
 if __name__ == "__main__":
+    # Run the Flask application
     app.run(host='0.0.0.0', port=5000)
